20732c19e80b8a040009752df5142ffc08eaeb29
Sagar Goswami <88379870+sagargoswami2001@users.noreply.github.com>
2022-03-04 00:08:55 +0530

    Mini Project

---
+++ Bitcoin_Mining.py
@@ -0,0 +1,34 @@
+# Python Program to Bitcoin Mining Process.
+
+from hashlib import sha256  # SHA256 is a Cryptographic Hash Function
+MAX_NONCE = 1000000000000
+
+def SHA256(text):
+    return sha256(text.encode("ascii")).hexdigest()
+
+def mine(block_number, transactions, previous_hash, prefix_zeros):
+    prefix_str = '0'*prefix_zeros
+    for nonce in range(MAX_NONCE):
+        text = str(block_number) + transactions + previous_hash + str(nonce)
+        new_hash = SHA256(text)
+        if new_hash.startswith(prefix_str):
+            print(f"\nHooray! Successfully Mined Bitcoins with Nonce Value: {nonce}")
+            return new_hash
+
+    raise BaseException(f"Couldn't find Correct has After Trying {MAX_NONCE} Times")
+
+if __name__=='__main__':
+    transactions = '''
+    Dhaval->Bhavin->20
+    Mando->Cara->45
+    '''
+
+    difficulty = 5 # Try Changing This to Higher Number and You Will See It Will Take More Time for Mining as Difficulty Increases.
+
+    import time
+    start = time.time()
+    print("\nStart Mining...")
+    new_hash = mine(5, transactions, '0000000xa036944e29568d0cff17edbe038f81208fecf9a66be9a2b8321c6ec7', difficulty)
+    total_time = str((time.time() - start))
+    print(f"End Mining. Mining Took: {total_time} Seconds")
+    print(new_hash)
